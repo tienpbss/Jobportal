781fb0c6c5f16e52651a3018d9454df970fdf626
tienpbss <tienphung.cn07@gmail.com>
2025-11-27 08:42:43 +0700

    init project

---
+++ src/main/java/com/spring/jobportal_redo/JobportalRedoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jobportal_redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalRedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalRedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jobportal_redo/JobportalRedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jobportal_redo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalRedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
