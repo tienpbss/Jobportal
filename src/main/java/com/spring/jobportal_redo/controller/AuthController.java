3454c7b38ce736b0c8240f7b78cd2297925db070
tienpbss <tienphung.cn07@gmail.com>
2025-12-06 10:16:36 +0700

    add /api/v1

--- src/main/java/com/spring/jobportal_redo/controller/AuthController.java
+++ src/main/java/com/spring/jobportal_redo/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.spring.jobportal_redo.controller;
 
 import com.spring.jobportal_redo.domain.dto.JwtResponseDto;
 import com.spring.jobportal_redo.domain.dto.LoginDto;
+import com.spring.jobportal_redo.service.UserService;
 import com.spring.jobportal_redo.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +12,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponseDto> login(@RequestBody LoginDto loginDto) {
--- src/main/java/com/spring/jobportal_redo/controller/CompanyController.java
+++ src/main/java/com/spring/jobportal_redo/controller/CompanyController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/companies")
+@RequestMapping("/api/v1/companies")
 public class CompanyController {
     private final CompanyService companyService;
     // GET all companies
