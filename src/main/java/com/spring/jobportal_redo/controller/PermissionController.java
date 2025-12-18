25d2f2fc4dc0871bb48fa8f352637347cc1d2baa
tienpbss <tienphung.cn07@gmail.com>
2025-12-18 16:11:02 +0700

    module permission

--- src/main/java/com/spring/jobportal_redo/controller/PermissionController.java
+++ src/main/java/com/spring/jobportal_redo/controller/PermissionController.java
@@ -14,8 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/permissions")
 @RequiredArgsConstructor
--- src/main/java/com/spring/jobportal_redo/domain/dto/permission/PermissionResponseDto.java
+++ src/main/java/com/spring/jobportal_redo/domain/dto/permission/PermissionResponseDto.java
@@ -1,15 +1,11 @@
 package com.spring.jobportal_redo.domain.dto.permission;
 
-import com.spring.jobportal_redo.domain.Role;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Setter
